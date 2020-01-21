58cf5ac8191a664a26c03320184a57055fa22c51
Craig Shih <cshih1141@gmail.com>
2020-01-21 14:21:09 -0800

    basic api request from coindesk

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.get('/retrievePrices', (req, res, next) => {
+
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+  .then((response) => {
+    // handle success
+    // console.log(response.data);
+    res.send(response.data);
+    next();
+  })
+  .catch((error) => {
+    // handle error
+    console.log(error);
+  });
+});
 
 app.listen(3000);
